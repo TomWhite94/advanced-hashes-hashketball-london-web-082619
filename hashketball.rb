9d9d9e7f00b0f029795159272bfb9fd0c42b6f9d
Tom White <tomjameswhite94@gmail.com>
2019-08-22 22:47:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -165,7 +165,7 @@ def player_numbers(team_names)
   game_hash.each do |place, team|
     if team[:team_name] == team_name
       team.each do |attributes, data|
-        if attributes == :players_name
+        if attributes == :players
           data.each do |player|
            nums << player[:number]
       end
