f700c66413fd91bcc60ed4effaed0e428081e38d
Tom White <tomjameswhite94@gmail.com>
2019-08-22 23:02:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -184,6 +184,7 @@ def player_stats(players_name)
         data.each do |player|
           if player[:player_name] = players_name
             new_hash = player.delete_if do |k, v]
+            k ==
         
     end
   end
