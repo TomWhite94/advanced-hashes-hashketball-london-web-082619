7d4f5cb6a74334e6ae913318a5f806f49209c487
Tom White <tomjameswhite94@gmail.com>
2019-08-22 23:08:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -176,13 +176,13 @@ end
 nums
 end
 
-def player_stats(players_name)
+def player_stats(player_name)
   new_hash = {}
   game_hash.each do |place, team|
     team.each do |attributes, data|
       if attributes == :players
         data.each do |player|
-          if player[:players_name] = players_name
+          if player[:players_name] = player_name
             new_hash = player.delete_if do |k, v|
             k == :players_name
           end
